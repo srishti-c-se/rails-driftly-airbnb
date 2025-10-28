2872184b018908b97d00f590ead887fbdc2deb3b
Vikesh Rooplall <rooplallv50@gmail.com>
2025-10-28 16:22:14 +0400

    added associations and validations to vehicle

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,13 @@
 class Vehicle < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
+  has_many :reviews,  dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
+  has_many :availabilities, dependent: :destroy
+
+  validates :title, :address, presence: true
+  validates :price_per_day, numericality: { greater_than: 0 }, allow_nil: true
+  validates :seats, numericality: { greater_than: 0 }, allow_nil: true
+  enum :transmission, { manual: 0, automatic: 1 }, prefix: true
+  enum :fuel_type,    { petrol: 0, diesel: 1, hybrid: 2, electric: 3 }, prefix: true
 end
