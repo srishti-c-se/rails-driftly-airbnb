d0f46ebcbdd4a845be988ecc4d8178c2114e6d8f
srishti chamilall <srishtichamilall14@gmail.com>
2025-10-28 14:04:55 +0400

    Create User model with Devise, attributes, validations, and associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_27_170931) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_28_094131) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -19,6 +19,13 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_27_170931) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "phone"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
